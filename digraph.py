633f28b8241848153e8f988bfc6dc8ea04cc69a4
Динар <45795277+dMau5@users.noreply.github.com>
2019-03-06 11:39:08 +0300

    Add files via upload

--- digraph.py
+++ digraph.py
@@ -1,8 +1,11 @@
-from networkx import Digraph
+import networkx as nx
 from redict import CGRdbDict
+from CGRdb import load_schema
+db = load_schema('sandbox', user='postgres', password='jyvt0n3', host='localhost', database='postgres')
 
 
-class CGRdbDigraph(DiGraph):
+class CGRdbDigraph(nx.DiGraph):
+    CGRdbDict.attached = db
     node_dict_factory = CGRdbDict
     adjlist_outer_dict_factory = CGRdbDict
     adjlist_inner_dict_factory = CGRdbDict
--- redict.py
+++ redict.py
@@ -2,15 +2,18 @@ from CGRtools.containers import MoleculeContainer
 from collections import MutableMapping
 from itertools import chain
 from pony.orm import db_session
-from CGRdb import Molecule
 from CGRdbUser import User
 
 
 class CGRdbDict(MutableMapping):
+    attached = False
 
     def __init__(self):
         self._dict = {}
         self._molecule_dict = {}
+
+    def __call__(self, *args, **kwargs):
+        pass
 
     def __getitem__(self, key):
         if isinstance(key, MoleculeContainer):
@@ -21,8 +24,8 @@ class CGRdbDict(MutableMapping):
     def __setitem__(self, key, value):
         if isinstance(key, MoleculeContainer):
             with db_session:
-                if not Molecule.structure_exists(key):
-                    Molecule(key, User[1])
+                if not self.attached.Molecule.structure_exists(key):
+                    self.attached.Molecule(key, User[1])
             self._molecule_dict[bytes(key)] = value
         else:
             self._dict[key] = value
@@ -34,7 +37,7 @@ class CGRdbDict(MutableMapping):
             del self._dict[key]
 
     def __iter__(self):
-        return chain(self._dict, (Molecule.find_structure(x).structure for x in self._molecule_dict))
+        return chain(self._dict, (self.attached.Molecule.find_structure(x).structure for x in self._molecule_dict))
 
     def __len__(self):
         return len(self._dict) + len(self._molecule_dict)

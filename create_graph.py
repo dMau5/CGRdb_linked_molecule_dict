b55ea7c7603334f48cd8b4f0f6b74948e0d47695
Динар <45795277+dMau5@users.noreply.github.com>
2019-03-07 13:59:30 +0300

    Add files via upload

--- create_graph.py
+++ create_graph.py
@@ -43,47 +43,47 @@ if __name__ == '__main__':
         print('work')
         # g = CGRdbDigraph()
         inp = Queue()
-        for x in islice(reactions, 100):
+        for x in reactions:
             inp.put(x)
         for _ in range(12):
             Process(target=worker, args=inp).start()
 
-        n = 0
-        while True:
-            sleep(1)
-            q1 = inp.qsize()
-            if q1:
-                # print('do output', q1, q2)
-                for _ in range(q1):
-                    r = inp.get()
-                    # g.add_node(n, data=[r.meta['source_id'], r.meta['text']])
-                    for reactant in r.reactants:
-                        # if isinstance(reactant, MoleculeContainer):
-                        if not Molecule.structure_exists(reactant):
-                            Molecule(reactant, User[1])
-                        # g.add_edge(reactant, n)
-                    for product in r.products:
-                        if not Molecule.structure_exists(product):
-                            Molecule(product, User[1])
-                        # g.add_edge(n, product)
-                    n += 1
-                    if not n % 100:
-                        print(f'--------{n} done--------')
-                        # break
-                    try:
-                        inp.put(next(reactions))
-                    except StopIteration:
-                        break
-                else:
-                    continue
-                break
-            # elif not q2:
-            #     # print('do input')
-            #     for e in islice(reactions, 100):
-            #         inp.put(e)
-
-        for _ in range(12):
-            inp.put('STOP')
+        # n = 0
+        # while True:
+        #     sleep(1)
+        #     q1 = inp.qsize()
+        #     if q1:
+        #         # print('do output', q1, q2)
+        #         for _ in range(q1):
+        #             r = inp.get()
+        #             # g.add_node(n, data=[r.meta['source_id'], r.meta['text']])
+        #             for reactant in r.reactants:
+        #                 # if isinstance(reactant, MoleculeContainer):
+        #                 if not Molecule.structure_exists(reactant):
+        #                     Molecule(reactant, User[1])
+        #                 # g.add_edge(reactant, n)
+        #             for product in r.products:
+        #                 if not Molecule.structure_exists(product):
+        #                     Molecule(product, User[1])
+        #                 # g.add_edge(n, product)
+        #             n += 1
+        #             if not n % 100:
+        #                 print(f'--------{n} done--------')
+        #                 # break
+        #             try:
+        #                 inp.put(next(reactions))
+        #             except StopIteration:
+        #                 break
+        #         else:
+        #             continue
+        #         break
+        #     # elif not q2:
+        #     #     # print('do input')
+        #     #     for e in islice(reactions, 100):
+        #     #         inp.put(e)
+        #
+        # for _ in range(12):
+        #     inp.put('STOP')
     # with open('biggest_graph.pickle', 'wb') as file:
     #     pickle.dump(g, file)
     #     print('URAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
